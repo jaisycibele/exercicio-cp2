9ca7e0e67e77689fd046affe41ca1ffcb0b8cff5
jaisycibele <jaisycibele@gmail.com>
2023-09-05 11:00:17 -0300

    Craindo o Router Provider

--- src/main.jsx
+++ src/main.jsx
@@ -8,6 +8,13 @@ import Produtos from './routes/Produtos/index.jsx'
 import Error from './routes/Error/index.jsx'
 import EditarProduto from './routes/editarProdutos/index.jsx'
 
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
+
+
 const router = createBrowserRouter([
   {
     path: "/",
